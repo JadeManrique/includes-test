bf5eff117a0e85ac201fc51a9c2ff14ead7c30d2
Jade Manrique <jade.a.manrique@gmail.com>
2018-05-07 09:57:43 -0700

    everything

---
+++ scripts/scripts.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+
+  function myFunction() {
+    var str = "Hello world, welcome to the universe.";
+    var n = str.includes("world");
+    document.getElementById("demo").innerHTML = n;
+
+});
